ab4efb9757e67744f5b0589d8ad8a88be041dada
Pratham Kapoor <97790689+PrathamK78@users.noreply.github.com>
2024-03-18 12:51:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -113,6 +113,6 @@ app.post('/bfhl', (req, res) => {
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(Server is running on port ${PORT});
+    console.log(`Server is running on port ${PORT}`);
 });
 // });
